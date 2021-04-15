0f653cf62b8c344c0164a30363a971f5a8843458
rippingpants <35607651+rippingpants@users.noreply.github.com>
2021-04-15 09:44:17 -0500

    Update _Amazonrequests.bat

---
+++ _Amazonrequests.bat
@@ -0,0 +1,2 @@
+REM This code is work in progress. Do not ask for support in Discord running this code.
+cmd /k pipenv run python app.py amazonrequests --offerid=[OoH153B8COAJADOSg8Te%2Ff3Cyr5RPOI3LTXTnIDCrEfU1n5vb9fa5aWaZ7Ckrm9d%2BILU%2F2D4Gb0%2FAGpcRqFKvRMtANUfd2wXpFZ9YwrTzcjTVvfKx1t6ubJ6I2705yZYjPYbvVdTP7sVDLTlz3Ykbw%3D%3D]
